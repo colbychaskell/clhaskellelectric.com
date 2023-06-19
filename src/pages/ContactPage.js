323686c837bca19469d384da2dc9e3b5dd4d54f1
Colby Haskell <cchaskel@usc.edu>
2023-06-18 21:11:01 -0400

    Fix address

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -12,11 +12,11 @@ const ContactPage = () => {
           <h5 className='mb-4'>Feel free to fill out our form or contact us directly</h5>
           {/* table of contact information  */}
           <p><b>Phone:</b> 207-367-2715</p>
-          <p><b>Fax:</b> 207-367-2716</p>
+          <p><b>Fax:</b> 207-367-2585</p>
           <p><b>Email:</b> <a href="mailto:clhaskell@clhaskellelectric.com" target="_blank" rel="noreferrer">clhaskell@clhaskellelectric.com</a>
           </p>
           <p><b>Mailing Address: </b>P.O. Box 43, Stonington, ME 04681</p>
-          <p><b>Physical Address: </b>76 Oceanville Rd, Stonington, ME 04681</p>
+          <p><b>Physical Address: </b>78 Oceanville Rd, Stonington, ME 04681</p>
 
           <h1 className='mt-5 mb-3'>Billing Information</h1>
           <p>Please mail checks to P.O. Box 43, Stonington, ME 04681. This is our only <u>Mailing Address</u>.</p>
